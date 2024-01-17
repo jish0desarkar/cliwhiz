459975dcfee704007a9f31b3ca38553fe10bdff1
Jishnu De Sarkar <desarkarjishnu@gmail.com>
2024-01-17 16:22:35 +0530

    Removed unwanted exports

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 #!/usr/bin/env node
 
-// Import the module './src/cmdaid.js'
+// // Import the module './src/cmdaid.js'
 import * as cmdaidModule from './src/cmdaid.js';
-
-// Export the contents of the imported module
-export default cmdaidModule;
